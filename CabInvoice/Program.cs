dffbf1cc6daf0d36340612e8327a5aac872f48ab
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-02-12 23:04:27 +0530

    [Sartaj] : added Calculate fare in cab invoice generator

---
+++ CabInvoice/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CabInvoice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Cab Invoice Generator Program");
+
+            CabInvoiceGen cabInvoiceGenerator = new CabInvoiceGen(RideType.NORMAL);
+            Console.WriteLine(cabInvoiceGenerator.CalculateFare(10, 15));
+            Console.ReadLine();
+        }
+    }
+}
